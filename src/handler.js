15f3d91fdb2e087a1be40e5c88d151a67c057fe6
Talgat S <talgat.s@protonmail.com>
2019-11-16 02:28:30 -0800

    Add sumOfTrips and solve problems 6-7

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,6 @@
 const generateGraph = require("./generateGraph");
 const calculateRouteDistance = require("./calculateRouteDistance");
+const { sumOfTrips, EXACT, MAX } = require("./sumOfTrips");
 
 function handler(data = "") {
 	const result = [];
@@ -12,6 +13,10 @@ function handler(data = "") {
 	result.push(calculateRouteDistance(graph, "ADC"));
 	result.push(calculateRouteDistance(graph, "AEBCD"));
 	result.push(calculateRouteDistance(graph, "AED"));
+
+	// 6 - 7
+	result.push(sumOfTrips(graph, "C", "C", 3, MAX));
+	result.push(sumOfTrips(graph, "A", "C", 4, EXACT));
 
 	return result;
 }
